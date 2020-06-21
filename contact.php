c4e5e134e020d1250d317a7f71ba016fd730c1e1
jobinpjoseph707 <54855287+jobinpjoseph707@users.noreply.github.com>
2020-06-21 12:52:19 +0530

    php compleat

--- contact.php
+++ contact.php
@@ -1,13 +1,17 @@
 <?php
-$to="jobinpjoseph006@gmail.com";
-$subject="respons";
-$message="massage";
-$headesr="";
+ $name = $_REQUEST['name'];
+ $email = $_REQUEST['email'];
+ $message = $_REQUEST['comt'];
 
-if(mail($to,$subjet,$message,$heders)){
-echo "mail sent";
+if(empty($name) || empty($email)  || empty($message))
+{
+	echo "please fill all";
 }
-else{
-echo "canot sent";
+else
+{
+	mail("jobinpjoseph006@gmail.com", "money", $message,"from: $name < $email >");
+	echo "<script type='text/javascript'>alert('your message sent succesfully');
+	window.history.log(-1);
+	</script>"
 }
 ?>
