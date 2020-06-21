29ad17fc75c195de9082364ae9a461f14fe9fadb
jobinpjoseph707 <54855287+jobinpjoseph707@users.noreply.github.com>
2020-06-21 11:44:28 +0530

    php demo

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$to="jobinpjoseph006@gmail.com";
+$subject="respons";
+$message="massage";
+$headesr="";
+
+if(mail($to,$subjet,$message,$heders)){
+echo "mail sent";
+}
+else{
+echo "canot sent";
+}
+?>
